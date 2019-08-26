7888fa30e3209bfbc04b487b21dc34b0e620afd5
xia_xun <1529068093@qq.com>
2019-08-26 21:17:11 +0800

    commit

---
+++ amon-core/src/main/java/com/githup/liuyanggithup/amon/exception/AmonException.java
@@ -0,0 +1,13 @@
+package com.githup.liuyanggithup.amon.exception;
+
+/**
+ * @author: xia_xun
+ * @Date: 2019/8/26
+ * @description:
+ */
+public class AmonException extends RuntimeException {
+
+    public AmonException(String msg) {
+        super(msg);
+    }
+}
